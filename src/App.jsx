0d9dd50371ffe1b688707c728b1d6883096b57f3
gotou1212 <meijiyou.20081212@gmail.com>
2025-03-27 08:40:26 +0000

    a

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,22 @@ function App() {
   setCount(count-1);
  };
 
+ const reset = () => {
+  setCount(0);
+ }
+
  return(
    <div className= "zenntai">
+    <h1 className="mozi">カウンターアプリ</h1>
      <p className= "count">{count}</p>
       <div>
        <button onClick={increment} className="age">↑</button>
        <button onClick={decrement} className="sage">↓</button>
       </div>
+    <div>
+     <button onClick={reset} className="reset">リセット</button>
     </div>
+   </div>
  );
 }
 
